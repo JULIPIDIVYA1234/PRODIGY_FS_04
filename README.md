aeb638f4630a3a6da85763b8e3e5b20f6920d6cf
JULIPIDIVYA1234 <146820386+JULIPIDIVYA1234@users.noreply.github.com>
2024-10-22 10:34:38 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# PRODIGY_FS_04
+# Prodigy Infotech Internship - Task 4
+
+This is a Chat Application built using the MERN stack and Socket.IO, allowing real-time messaging between users.
---
+++ pythonpractice.py
@@ -0,0 +1,7 @@
+x=int(input("read value for x:"))
+y=int(input("read value for y:"))
+print("values for x and y before swaping", x,y)
+t=x
+x=y
+y=t
+print("values for x and y after swaping", x,y)
